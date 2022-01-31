764d97f82ea24745b06ba0d36c9efa7adb511251
zonkje <szymon.radziszewski93@gmail.com>
2022-01-31 16:44:45 +0100

    closes #4

--- java-basics/src/com/company/Human.java
+++ java-basics/src/com/company/Human.java
@@ -1,9 +1,13 @@
 package com.company;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Human extends Animal{
 
     private Animal pet;
     private Car car;
+    private Double salary;
 
     public Human() {
         super("mammals");
@@ -24,5 +28,24 @@ public class Human extends Animal{
     public void setCar(Car car) {
         this.car = car;
         car.setOwner(this);
+    }
+
+    public Double getSalary() {
+        SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+        Date date = new Date(System.currentTimeMillis());
+        System.out.println("Checking salary. date: "+ formatter.format(date));
+        System.out.println("Your salary is $"+salary);
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        if(salary < 0){
+            System.out.println("New salary must be non-negative");
+            return;
+        }
+        System.out.println("New data has been sent to the accounting system");
+        System.out.println("Go to the HR department and pick up the contract amendment from Mrs. Hania");
+        System.out.println("We have sent information about your new salary to the ZUS and US");
+        this.salary = salary;
     }
 }
